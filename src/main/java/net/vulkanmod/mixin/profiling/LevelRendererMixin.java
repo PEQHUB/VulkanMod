4c33899d942d08627fea2ed6c55f86a29cf718f5
PEQHUB <peqhub@users.noreply.github.com>
2026-02-23 22:39:57 -0600

    Fix profiling LevelRendererMixin: add long param to method_62205, remove Frustum from method_62214

--- src/main/java/net/vulkanmod/mixin/profiling/LevelRendererMixin.java
+++ src/main/java/net/vulkanmod/mixin/profiling/LevelRendererMixin.java
@@ -2,11 +2,8 @@ package net.vulkanmod.mixin.profiling;
 
 import com.mojang.blaze3d.buffers.GpuBufferSlice;
 import com.mojang.blaze3d.resource.ResourceHandle;
-import net.minecraft.client.Camera;
 import net.minecraft.client.CloudStatus;
-import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.client.renderer.state.LevelRenderState;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.phys.Vec3;
@@ -21,18 +18,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class LevelRendererMixin {
 
     @Inject(method = "method_62205", at = @At("HEAD"))
-    private void pushProfiler(int i, CloudStatus cloudStatus, float f, Vec3 vec3, float g, CallbackInfo ci) {
+    private void pushProfiler(int i, CloudStatus cloudStatus, float f, Vec3 vec3, long l, float g, CallbackInfo ci) {
         Profiler profiler = Profiler.getMainProfiler();
         profiler.push("Clouds");
     }
 
     @Inject(method = "method_62205", at = @At("RETURN"))
-    private void popProfiler(int i, CloudStatus cloudStatus, float f, Vec3 vec3, float g, CallbackInfo ci) {
+    private void popProfiler(int i, CloudStatus cloudStatus, float f, Vec3 vec3, long l, float g, CallbackInfo ci) {
         Profiler profiler = Profiler.getMainProfiler();
         profiler.pop();
     }
 
-    // TODO: fix
     @Inject(method = "method_62213", at = @At(value = "HEAD"))
     private void pushProfiler3(GpuBufferSlice gpuBufferSlice, ResourceHandle resourceHandle,
                                ResourceHandle resourceHandle2, CallbackInfo ci) {
@@ -52,12 +48,11 @@ public class LevelRendererMixin {
                      target = "Lnet/minecraft/client/renderer/LevelRenderer;submitEntities(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/state/LevelRenderState;Lnet/minecraft/client/renderer/SubmitNodeCollector;)V"))
     private void profilerTerrain2(GpuBufferSlice gpuBufferSlice, LevelRenderState levelRenderState,
                                   ProfilerFiller profilerFiller, Matrix4f matrix4f, ResourceHandle resourceHandle,
-                                  ResourceHandle resourceHandle2, boolean bl, Frustum frustum,
+                                  ResourceHandle resourceHandle2, boolean bl,
                                   ResourceHandle resourceHandle3, ResourceHandle resourceHandle4, CallbackInfo ci) {
         Profiler profiler = Profiler.getMainProfiler();
         profiler.pop();
         profiler.push("entities");
     }
-
 
 }
